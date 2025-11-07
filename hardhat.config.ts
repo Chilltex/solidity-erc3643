796b66f68262d9b5264b215e664fd31e1ec2e37c
gpablo6 <gpablo6@outlook.com>
2025-11-07 06:32:30 -0600

    config: Include hardhat config and update dependencies

---
+++ hardhat.config.ts
@@ -0,0 +1,52 @@
+import "@nomicfoundation/hardhat-ethers";
+import "@nomicfoundation/hardhat-chai-matchers";
+import "@openzeppelin/hardhat-upgrades";
+import "@typechain/hardhat";
+import dotenv from "dotenv";
+import type { HardhatUserConfig } from "hardhat/config";
+
+dotenv.config();
+
+const config: HardhatUserConfig = {
+  defaultNetwork: "hardhat",
+  networks: {
+    hardhat: {
+      accounts: {},
+    },
+    sepolia: {
+      url: `https://eth-sepolia.g.alchemy.com/v2${process.env.ALCHEMY_KEY}`,
+      chainId: 11155111,
+      accounts: [process.env.WALLET_PK || ""],
+    },
+    ethereum: {
+      url: `https://eth-mainnet.g.alchemy.com/v2${process.env.ALCHEMY_KEY}`,
+      chainId: 1,
+      accounts: [process.env.WALLET_PK || ""],
+    },
+  },
+  paths: {
+    artifacts: "./artifacts",
+    cache: "./cache",
+    sources: "./contracts",
+    tests: "./tests",
+  },
+  typechain: {
+    outDir: "./typechain",
+    target: "ethers-v6",
+  },
+  solidity: {
+    compilers: [
+      {
+        version: "0.8.18",
+        settings: {
+          optimizer: {
+            enabled: true,
+            runs: 500,
+          },
+        },
+      },
+    ],
+  },
+};
+
+export default config;
